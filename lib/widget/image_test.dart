b738072e66d2b7c1852edf33fbd8b2a957117354
Diamond Cat <uyuuki0919@icloud.com>
2021-02-15 16:14:57 +0900

    change image

--- lib/widget/image_test.dart
+++ lib/widget/image_test.dart
@@ -3,12 +3,10 @@ import 'package:flutter/material.dart';
 class ImageTestWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      'https://pbs.twimg.com/media/Et1O05-VIAQPdb1?format=jpg&name=medium',
-      height: double.infinity,
-      width: double.infinity,
-      fit: BoxFit.cover,
-      alignment: Alignment.center,
+    return Center(
+      child: Image.network(
+        'https://pbs.twimg.com/media/EooWa1EU8AAuPD3?format=jpg&name=large',
+      ),
     );
   }
 }
